782e4be5dd17997eb3926df314ac019b59c4fa77
Dennis Garding <d.garding@shopware.com>
2025-01-12 14:21:47 +0100

    feat: add create link category

---
+++ admin/src/views/Link/LinkCreate.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: 'LinkCreate',
+
+  setup() {
+
+  }
+}
+</script>
+
+<template>
+<div class="row page-head">
+  <div class="col-6">
+    <h1>Create Link</h1>
+  </div>
+  <div class="col-6">
+
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- admin/src/views/LinkView.vue
+++ admin/src/views/LinkView.vue
@@ -1,15 +1,42 @@
 <script>
+import Router from "@/router/index.js";
+
 export default {
   name: 'LinkView',
 
   setup() {
 
+  },
+
+  methods: {
+    onAddLink() {
+      Router.push({ name: 'LinkCreate' });
+    },
+
+    onAddCategory() {
+      Router.push({ name: 'LinkCategoryCreate' });
+    }
   }
 }
 </script>
 
 <template>
-<h1>Link View</h1>
+  <div class="row page-head">
+    <div class="col-6">
+      <h1>Links</h1>
+    </div>
+    <div class="col-6">
+      <button class="btn btn-light mt-2" @click="onAddLink">
+        <i class="bi bi-plus-lg"></i>
+        Link
+      </button>
+
+      <button class="btn btn-light mt-2" @click="onAddCategory">
+        <i class="bi bi-plus-lg"></i>
+        Category
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
